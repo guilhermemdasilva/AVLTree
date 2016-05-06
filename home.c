4fa839de1093ad339779196632c7f86a264558a1
Guilherme Monteiro <guilherme.mdasilva@gmail.com>
2016-05-06 16:41:54 -0400

    added initialize and insert operations

--- home.c
+++ home.c
@@ -1,5 +1,70 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct Node {
+    struct Node *right;
+    struct Node *left;
+    int value;
+} Node;
+
+typedef struct {
+    Node *root;
+} AVLTree;
+
+AVLTree *tree = NULL;
+
+void printTree(Node *next) {
+    if(next != NULL) {
+        printTree(next->left);
+        printf("%d ", next->value);
+        printTree(next->right);
+    }
+}
+
+void insertNode(int value) {
+    Node *aux = tree->root;
+    Node *new = NULL;
+    new = (Node*)malloc(sizeof(Node));
+    new->right = NULL;
+    new->left = NULL;
+    new->value = value;
+    if (aux == NULL) {
+        tree->root = new;
+        return;
+    }
+    while(1) {
+        if(aux->value > value) {
+            if(aux->left != NULL) {
+                aux = aux->left;
+            } else {
+                aux->left = new;
+                break;
+            }
+        } else {
+            if(aux->right != NULL) {
+                aux = aux->right;
+            } else {
+                aux->right = new;
+                break;
+            }
+        }
+    }
+}
+
+void initializeTree() {
+    int i, r;
+    tree = (AVLTree*)malloc(sizeof(AVLTree));
+    tree->root = NULL;
+    srand(time(NULL));
+    for(i = 0; i < 10; i++) {
+        r = rand() % 50;
+        insertNode(r);
+    }
+}
 
 main() {
-    printf("Dakkon nosso rei\n");
+    initializeTree();
+    printTree(tree->root);
+    printf("\n");
 }
